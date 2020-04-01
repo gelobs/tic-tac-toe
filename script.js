9ead27ff177c4ed2bbf29dd4d2dbd213e4953c0f
Angelos Bousis <bousis.ang@gmail.com>
2020-04-01 10:19:52 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Gameboard module
+const gameBoard = ((selection)=>{
+    let gameboard=['O', 'O', 'X', 'X','X','O','X','O','O'];
+    // gameboard.push(selection);
+})();
+
+// Display Controller module
+const displayController =(()=>{
+
+})();
+
+// Player 
+const Player = (name) => {
+    return {name};
+}
