4e3d990b6646a0a4bc539c00a0b3d02fe6f4728d
Angelos Bousis <bousis.ang@gmail.com>
2020-04-03 10:34:31 +0300

    Some changes

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 // Gameboard module
-const gameBoard = ((selection)=>{
-    let gameboard=['O', 'O', 'X', 'X','X','O','X','O','O'];
-    // gameboard.push(selection);
+const gameBoard = ((mark, index)=>{
+    const gameboard=['', '', '', '','','','','',''];
+    gameboard[index]=mark;
+    return gameboard;
 })();
 
 // Display Controller module
 const displayController =(()=>{
-
+    let id;
+    let containerItems = document.querySelectorAll('.container-item');
+    for(let i=0; i < containerItems.length; i++){
+        containerItems[i].addEventListener('click', (e) => {
+            console.log(e.target.id);
+            id = e.target.id;
+        }) 
+    }
+    return id;
 })();
 
 // Player 
-const Player = (name) => {
-    return {name};
+const Player = (name, mark) => {
+    let id = displayController;
+    console.log(id);
+    return {name, mark, id};
 }
+
+// render function
+function render(player){
+    var containerItems = document.querySelectorAll('.container-item');
+    var gameboard = gameBoard(player.mark, player.id);
+    for(var i=0; i < containerItems.length; i++){
+        containerItems[i].textContent = gameboard[i];
+    }
+}
+const player1 = Player('Miranda', 'O');
+const player2 = Player('Sotos', 'X');
+
+
+render(player1);
+
